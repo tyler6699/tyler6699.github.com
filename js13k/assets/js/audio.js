a10877cbc355d20a5889fd649a3e4804eda7a8a0
Ryan Tyler <ryan.tyler@southwales.ac.uk>
2019-08-15 19:51:33 +0100

    Remove sound wav files, clean up audio class.

--- js13k/assets/js/audio.js
+++ js13k/assets/js/audio.js
@@ -16,31 +16,50 @@ function playSound(name){
 }
 
 getSound = function (name,i){
+  var val;
   switch(name) {
   case NOISEFX:
-    return Math.random() * 2 - 1;
+    val = getNoise(i);
     break;
   case COINFX:
-    var n=1e4;
-    var c=n/3;
-    if (i > n) return null;
-    var t = (n-i)/n;
-    var q=Math.pow(t,2.1);
-    return (Math.pow(i,3)&(i<c?16:99))?q:-q;
+    val = getCoin(i);
     break;
   case JUMPFX:
-    var n = 1e4;
-    if (i>n) return null;
-    var t = (n-i)/n;
-    return ((Math.pow(i,1.055)&128)?1:-1)*Math.pow(t,2);
+    val = getJump(i);
     break;
   case FALLFX:
-    var n=5e4;
-    if (i > n) return null;
-    var t = (n-i)/n;
-    return ((Math.pow(i,0.9)&200)?1:-1)*Math.pow(t,3);
+    val = getFall(i);
     break;
   default:
     return 1;
   }
+
+  return val;
+}
+
+function getNoise(i){
+  return Math.random() * 2 - 1;
+}
+
+function getJump(i){
+  var n = 1e4;
+  if (i>n) return null;
+  var t = (n-i)/n;
+  return ((Math.pow(i,1.055)&128)?1:-1)*Math.pow(t,2);
+}
+
+function getFall(i){
+  var n=5e4;
+  if (i > n) return null;
+  var t = (n-i)/n;
+  return ((Math.pow(i,0.9)&200)?1:-1)*Math.pow(t,3);
+}
+
+function getCoin(i){
+  var n=1e4;
+  var c=n/3;
+  if (i > n) return null;
+  var t = (n-i)/n;
+  var q=Math.pow(t,2.1);
+  return (Math.pow(i,3)&(i<c?16:99))?q:-q;
 }
--- js13k/assets/js/media.js
+++ js13k/assets/js/media.js
@@ -2,6 +2,3 @@ var wall = "assets/wall.png";
 var brick = "assets/brick.png";
 var coin = "assets/coin.png";
 var ledge = "assets/ledge.png";
-// http://github.grumdrig.com/jsfxr
-var coinSFX = new Audio("assets/coin.wav");
-var jumpSFX = new Audio("assets/jump.wav");
