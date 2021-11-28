aa89acb30101099a5a34ba4f8bd0e345bc64ef39
Ryan Tyler <tyler6699@gmail.com>
2021-11-28 01:12:15 +0000

    New website design

---
+++ js/content.js
@@ -0,0 +1,274 @@
+projects = [{
+  "id":"js13k-2021",
+  "captionHeading":"JS13k 2021",
+  "captionSubheading":"Space Kitty",
+  "imgSmall":"images/portfolio/spacekitty.png",
+  "imgBig":"images/portfolio/spacekitty.png",
+  "imgAlt":"A space kitty game.",
+  "categoryList":["gamejam","JS13k","JS"],
+  "body":"Twin shooter top down game, 29th position overall in 2021 JS13k gamejam.",
+  "actionURL":"https://js13kgames.com/entries/space-kitty",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"js13k-2020",
+  "captionHeading":"JS13k 2020",
+  "captionSubheading":"In To Net Cafe",
+  "imgSmall":"images/portfolio/intonetcafe.jpg",
+  "imgBig":"images/portfolio/intonetcafe.jpg",
+  "imgAlt":"A pixel art space cafe",
+  "categoryList":["gamejam","JS13k","JS"],
+  "body":"In To Net Cafe: A JS game that compresses to less than 13kb, 2020 entry is a simple building sim, came 52nd in the Jam.",
+  "actionURL":"https://js13kgames.com/entries/in-to-net-cafe",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"js13k-2019",
+  "captionHeading":"JS13k 2019",
+  "captionSubheading":"Quick Wins",
+  "imgSmall":"images/portfolio/quickwins.jpg",
+  "imgBig":"images/portfolio/quickwins.jpg",
+  "imgAlt":"view of a pixel art platformer level",
+  "categoryList":["gamejam","JS13k","JS"],
+  "body":"Quick Wins - First entry into the JS13K Game Jam, a platformer based on old Gameboy games.",
+  "actionURL":"https://js13kgames.com/entries/quick-wins",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"topdown-game-libgdx",
+  "captionHeading":"Topdown Game",
+  "captionSubheading":"LibGDX Experiment",
+  "imgSmall":"images/portfolio/topdown.gif",
+  "imgBig":"images/portfolio/topdown.gif",
+  "imgAlt":"Top view of car driving",
+  "categoryList":["topdown","Java","LibGDX"],
+  "body":"A work in progress 2D / 3D environment for a topdown GTA style game.",
+  "actionURL":"test",
+  "actionTitle":"test",
+  "actionFA":"fas fa-link"
+},
+{
+  "id":"libgdx-tutorials",
+  "captionHeading":"Tutorials",
+  "captionSubheading":"LibGDX game dev",
+  "imgSmall":"https://carelesslabs.files.wordpress.com/2017/09/bird_walking.gif?w=646",
+  "imgBig":"https://carelesslabs.files.wordpress.com/2017/09/bird_walking.gif?w=800",
+  "imgAlt":"2D island with a bird flying around",
+  "categoryList":["topdown","Java","LibGDX","Tutorial"],
+  "body":"Index page for all the tutorials I have created for LibGDX / Java.",
+  "actionURL":"https://carelesslabs.wordpress.com/tutorials/",
+  "actionTitle":"Visit",
+  "actionFA":"fab fa-wordpress-simple"
+},
+{
+  "id":"html5-learning",
+  "captionHeading":"HTML5 Experiment",
+  "captionSubheading":"Learning HTML5, JS gamedev",
+  "imgSmall":"images/portfolio/html5test.gif",
+  "imgBig":"images/portfolio/html5test.gif",
+  "imgAlt":"2D island with a bird flying around",
+  "categoryList":["topdown","JS","HTML5","Experiment"],
+  "body":"this is the body",
+  "actionURL":"https://carelesslabs.wordpress.com/tutorials/",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"libgdx-ui",
+  "captionHeading":"LibGDX Custom UI",
+  "captionSubheading":"On hold, custom ui elements",
+  "imgSmall":"images/portfolio/dropdown.gif",
+  "imgBig":"images/portfolio/dropdown.gif",
+  "imgAlt":"Animated UI boxes",
+  "categoryList":["LibGDX","Java","UI","Experiment"],
+  "body":"There are no simple LibGDX button, dropdown and other UI controls I liked so started to create my own.",
+},
+{
+  "id":"ludum-dare-33",
+  "captionHeading":"Monster Break Out",
+  "captionSubheading":"Ludum Dare 33 entry",
+  "imgSmall":"images/portfolio/ld33.png",
+  "imgBig":"images/portfolio/ld33.png",
+  "imgAlt":"Character stood in power room.",
+  "categoryList":["Ludum Dare","Java","Gamejam", "LibGDX"],
+  "body":"Ludum Dare 33 entry, escape a prison in this side view plaformer with simple puzzles.",
+  "actionURL":"https://carelesslabs.itch.io/monster-break-out",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"ludum-dare-32",
+  "captionHeading":"Light of the Living Dead",
+  "captionSubheading":"Ludum Dare 32 entry",
+  "imgSmall":"images/portfolio/ld32.png",
+  "imgBig":"images/portfolio/ld32.png",
+  "imgAlt":"Character stood in power room.",
+  "categoryList":["Ludum Dare","Java","Gamejam", "LibGDX"],
+  "body":"Ludum Dare 32 entry, walk around randomly generated rooms hunting out the baddies and frying them with your torch!",
+  "actionURL":"https://carelesslabs.itch.io/light-of-the-living-dead",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"bot-called-bod",
+  "captionHeading":"Bot Called Bod",
+  "captionSubheading":"Platformer experiment, cute animations, wall jumps, action replays and level building.",
+  "imgSmall":"images/portfolio/bot_bod.png",
+  "imgBig":"images/portfolio/bot_bod.png",
+  "imgAlt":"Robot shown within a game level",
+  "categoryList":["Experiment","Java","LibGDX"],
+  "body":"One of my early game tests within Java, would like to come back to this and rebuild in HTML5.",
+  "actionURL":"https://twitter.com/CarelessLabs/status/574005221107634177",
+  "actionTitle":"Twitter Gif",
+  "actionFA":"fab fa-twitter"
+},
+{
+  "id":"super-meatly-boy",
+  "captionHeading":"TheMeatly Jam",
+  "captionSubheading":"Super Meatly Boy",
+  "imgSmall":"images/portfolio/supermeatlyboy.png",
+  "imgBig":"images/portfolio/supermeatlyboy.png",
+  "imgAlt":"Super meatly jam image",
+  "categoryList":["Gamejam,","Processing","Javascript"],
+  "body":"To learn Processing JS I decided to enter this jam and make a Super Meat Boy game. Made is under 48 hours this game has 15 levels of jumping fun.",
+  "actionURL":"game/the_meatly_jam.html",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"follow-friday-game",
+  "captionHeading":"Twitter #FF Game",
+  "captionSubheading":"Super Meatly Boy",
+  "imgSmall":"images/portfolio/ffanim.gif",
+  "imgBig":"images/portfolio/ffanim.gif",
+  "imgAlt":"Super meatly jam image",
+  "categoryList":["Gamejam,","Processing","Javascript"],
+  "body":"Made for Twitter Follow Friday, select up to 10 users to include in this Mario style mini game. Hit the boxes to reveal your #FF users.",
+  "actionURL":"https://carelesslabs.co.uk/ff/",
+  "actionTitle":"Generate your own!",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"endure",
+  "captionHeading":"Endure",
+  "captionSubheading":"LibGDX isometric random island generation with map builder, working in progress (on hold).",
+  "imgSmall":"images/portfolio/endure.png",
+  "imgBig":"images/portfolio/endure.png",
+  "imgAlt":"Characte stood on the beach",
+  "categoryList":["LibGDX,","Java","Procedural"],
+  "body":"A multi-player** survival game set on a Island. Still in its early stages, currently has a private test server that allows players to explore. Working on a map editor to produce the final Island for the game. Check out IndieDB, the Blog and Twitter for updates. ** Multiplayer was tested as a proof of concept but never fully implemented.",
+  "actionURL":"https://carelesslabs.co.uk/ff/",
+  "actionTitle":"IndieDB",
+  "actionFA":"fas fa-database"
+},
+{
+  "id":"clicky-pixel",
+  "captionHeading":"Flappy Bird Jam",
+  "captionSubheading":"Clicky Pixel",
+  "imgSmall":"images/portfolio/clicky.png",
+  "imgBig":"images/portfolio/clicky.png",
+  "imgAlt":"Flappy bird style game screenshot",
+  "categoryList":["Gamejam,","Android","LibGDX"],
+  "body":"Skilfully tap your way through 4 levels Flappy Bird Style. Each gate you pass adds to your total progress, hit 100% to unlock the next level. Buy hearts and unlock character which decrease in size to help you on your quest to complete all 4 levels! On Windows or Mac use mouse or any key to jump, on Android just tap the screen. Needs patching and redeoloying to the app store.",
+},
+{
+  "id":"mech-stroller",
+  "captionHeading":"Cyber Junk Jam",
+  "captionSubheading":"Mech Stroller",
+  "imgSmall":"images/portfolio/mech.png",
+  "imgBig":"images/portfolio/mech.png",
+  "imgAlt":"Flappy bird style game screenshot",
+  "categoryList":["Gamejam,","Processing","Javascript"],
+  "body":"Mech Stroller brings back an old classic walk and shoot your way through wave attacks. Battle against several enemy types while not letting your gun overheat! Made in just over a week by Careless Labs this demo lets you test out the early prototype. If enough people like this then the whole game will be rewritten and loads more features added.",
+  "actionURL":"mech",
+  "actionTitle":"Play",
+  "actionFA":"fas fa-gamepad"
+},
+{
+  "id":"susan-balaka",
+  "captionHeading":"Susan Balaka (Indie Movie)",
+  "captionSubheading":"Game made for anniversairy",
+  "imgSmall":"images/portfolio/balaka.png",
+  "imgBig":"images/portfolio/balaka.png",
+  "imgAlt":"Flappy bird style game screenshot",
+  "categoryList":["Game,","Movie"],
+  "body":"10 year anniversairy game for @wingshootbag The game to the indie movie \"Susan Balaka\", not currently available to download. This game was made for friends to go into an events goody bag.",
+  "actionURL":"https://carelesslabs.wordpress.com/2014/09/07/wing-wing-shoot-bag-the-game-postmortum/",
+  "actionTitle":"Read More",
+  "actionFA":"fab fa-wordpress-simple"
+}
+];
+
+jQuery.each( projects, function(index,project) {
+  var projItem = `
+    <div class=\"col-lg-4 col-sm-6 mb-4\">
+        <!-- Portfolio item 1-->
+        <div class=\"portfolio-item\">
+            <a class=\"portfolio-link\" data-bs-toggle=\"modal\" href=\"#`+project.id+`\">
+                <div class=\"portfolio-hover\">
+                    <div class=\"portfolio-hover-content\"><i class=\"fas fa-plus fa-3x\"></i></div>
+                </div>
+                <img class=\"img-fluid\" src=\"`+project.imgSmall+`" alt=\"`+project.imgAlt+`\" />
+            </a>
+            <div class=\"portfolio-caption\">
+                <div class=\"portfolio-caption-heading\">`+project.captionHeading+`</div>
+                <div class=\"portfolio-caption-subheading text-muted\">`+project.captionSubheading+`</div>
+            </div>
+        </div>
+    </div>`;
+    $("#project-items").append(projItem);
+
+    <!-- Portfolio item 1 modal popup-->
+    categories = "";
+    action="";
+    if(project.actionURL){
+      action=`
+      <p>
+        <a href=\"`+project.actionURL+`\" class=\"btn btn-success btn\" type=\"button\" target=\"_blank\">
+          <i class=\"`+project.actionFA+`\"></i>
+          `+project.actionTitle+`
+          </a>
+      </p>
+      `
+    }
+    jQuery.each(project.categoryList, function(index,cat) {
+      categories += "<span class=\"badge bg-info text-dark\">"+ cat + "</span> ";
+    });
+
+    modal = `
+      <div class=\"portfolio-modal modal fade" id=\"`+project.id+`\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">
+        <div class=\"modal-dialog\">
+            <div class=\"modal-content\">
+                <div class=\"close-modal\" data-bs-dismiss=\"modal\"><img src=\"assets/img/close-icon.svg\" alt=\"Close modal\" /></div>
+                <div class="container">
+                    <div class=\"row justify-content-center\">
+                      <div class=\"col-lg-8\">
+                          <div class=\"modal-body\">
+                              <!-- Project details-->
+                              <h2 class=\"text-uppercase\">`+project.captionHeading+`</h2>
+                              <p class=\"item-intro text-muted\">`+project.captionSubheading+`</p>
+                              <img class=\"img-fluid d-block mx-auto\" src=\"`+project.imgBig+`\" alt=\"`+project.imgAlt+`\" />
+                              <p>`+project.body+`</p>
+                              `+action+`
+                              <ul class=\"list-inline\">
+                                <li><strong>Category:</strong>
+                                  `+categories+`
+                                </li>
+                              </ul>
+                              <button class=\"btn btn-primary btn-xl text-uppercase\" data-bs-dismiss=\"modal\" type=\"button\">
+                                <i class=\"fas fa-times me-1\"></i>
+                                Close Project
+                              </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  `;
+  $("#modal-list").append(modal);
+});
