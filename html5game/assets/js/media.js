c61e44c1ab99d6d7688fe86d8dad35d5156581d9
Ryan Tyler <tyler6699@gmail.com>
2025-03-30 22:04:43 +0100

    clear out files

--- html5game/assets/js/media.js
+++
@@ -1,2 +0,0 @@
-var water = "assets/8x8/water/water_01.png";
-var grass = "assets/8x8/grass/grass_04_32.png";
--- js13k/assets/js/camera.js
+++
@@ -1,8 +0,0 @@
-function Camera(x, y) {
-  this.entity = new entityObj(30, 30, x, y, "view");
-
-  this.newPos = function(hero, level) {
-    this.entity.x = parseInt( (hero.entity.x + hero.entity.width/2) - level.canvasHalfW );
-    this.entity.y = parseInt( ((hero.entity.y + hero.entity.height/2) - level.canvasHalfH) - offset );
-  }
-}
